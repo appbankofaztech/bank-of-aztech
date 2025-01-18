8473f21aaae474e73ddcd5c382b68bf4e33173f8
LFGenesis <birl1onavax@gmail.com>
2025-01-18 18:11:01 +0300

    Update build for main domain

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app.bankofaztech/sw.js', { scope: '/app.bankofaztech/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
