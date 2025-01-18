794a0ef96417bdef79ae707c535ddce8d12986a0
LFGenesis <birl1onavax@gmail.com>
2025-01-18 18:02:57 +0300

    Update RainbowKit configuration

--- vite.config.js
+++ vite.config.js
@@ -4,36 +4,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'owl-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Bank of Aztech',
-        short_name: 'BOFA',
-        description: 'Bank of Aztech dApp',
+        short_name: 'Bank of Aztech',
         theme_color: '#805AD5',
         background_color: '#1A202C',
+        display: 'standalone',
+        scope: '/app.bankofaztech/',
+        start_url: '/app.bankofaztech/',
         icons: [
           {
-            src: 'owl-icon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ]
       }
     })
   ],
-  resolve: {
-    alias: {
-      buffer: 'buffer/'
-    }
-  },
-  define: {
-    'process.env': {},
-    global: {}
-  }
+  base: '/app.bankofaztech/'
 }) 
